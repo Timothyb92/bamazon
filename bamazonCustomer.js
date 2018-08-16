9564c19d90344059728fa17492d729b2338aef23
Timothy Bedsole <timbedsole@gmail.com>
2018-08-16 19:03:21 -0400

    added console.table npm, connected to db and logging the items in the product table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+})
+
+connection.query("SELECT * FROM products", function(err, result){
+    console.table(result);
+})
