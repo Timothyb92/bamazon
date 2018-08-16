39465bd067aa3ea0efe598a9ecb320e7f2f8345b
Timothy Bedsole <timbedsole@gmail.com>
2018-08-16 19:11:14 -0400

    added logic to have inquirere ask user what ithem they want to purchase and how many

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,5 +11,21 @@ var connection = mysql.createConnection({
 })
 
 connection.query("SELECT * FROM products", function(err, result){
+    if (err) throw err;
     console.table(result);
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemNum",
+            message: "Enter the product ID number of the item you wish to purchase."
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function(answers){
+        console.log(answers.itemNum);
+        console.log(answers.quantity);
+    })
 })
